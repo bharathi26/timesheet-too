9e1b30024713869054618009d677c171aed8f6ae
Wayne Werner <wayne@waynewerner.com>
2013-02-07 21:04:30 -0600

    Got another test passing

--- tests/test_timesheet.py
+++ tests/test_timesheet.py
@@ -1,5 +1,7 @@
-from timesheet import Timesheet
 import unittest
+
+from datetime import datetime
+from timesheet import Timesheet
 
 class GivenNewTimesheet(unittest.TestCase):
 
@@ -21,12 +23,78 @@ class GivenNewTimesheet(unittest.TestCase):
             self.timesheet.add_time("1234")
 
 
-    def test_adding_time_with_HH_MM_SS_AM_should_not_ValueError(self):
+    def test_adding_time_with_HH_MM_AM_should_not_ValueError(self):
         self.timesheet.add_time("12:42 AM")
 
 
-class GivenTimesheetWithOneInterval(unittest.TestCase):
+    def test_adding_time_with_HH_MM_PM_should_not_ValueError(self):
+        self.timesheet.add_time("2:12 PM")
 
+
+    def test_adding_time_with_two_HH_MM_AM_should_not_error(self):
+        self.timesheet.add_time("3:13 AM", "5:17 PM")
+
+
+    def test_adding_time_with_good_start_bad_end_should_ValueError(self):
+        with self.assertRaises(ValueError):
+            self.timesheet.add_time("4:14 PM", "sdfkj23")
+
+    
+    def test_adding_time_with_good_start_good_end_should_not_error(self):
+        self.timesheet.add_time("12:00 AM", "3:33 AM")
+
+
+    def test_adding_time_with_good_start_end_and_any_task_should_not_error(self):
+        self.timesheet.add_time("12:00 AM", "4:44 PM", "fnord")
+
+
+    def test_adding_time_with_good_start_end_any_task_and_proj_should_not_error(self):
+        self.timesheet.add_time("1:43 AM", "4:12 AM", "fnord", "999")
+
+    def test_adding_time_with_good_start_thru_proj_and_bad_date_should_ValueError(self):
+        with self.assertRaises(ValueError):
+            self.timesheet.add_time("9:42 AM",
+                                    "9:59 AM",
+                                    "fnord",
+                                    "fizz",
+                                    "Silly non-date")
+
+
+    def test_adding_time_with_good_vals_and_good_date_should_not_error(self):
+        self.timesheet.add_time("8:12 AM",
+                                "8:13 AM",
+                                "fnord",
+                                "fizzy",
+                                "2010-08-14")
+
+
+class GivenTimesheetWithOneTaskWithStartAndEnd(unittest.TestCase):
     def setUp(self):
         self.timesheet = Timesheet()
-        self.timesheet.add_time()
+        self.timesheet.add_time("10:45 AM", "11:00 AM")
+
+
+    def test_it_should_use_current_date_as_default(self):
+        self.assertEqual(datetime.today().date(),
+                         self.timesheet.tasks[-1].date)
+
+
+    def test_current_task_should_be_None(self):
+        self.assertIsNone(self.timesheet.current_task)
+
+
+class GivenTimesheetWithOpenInterval(unittest.TestCase):
+
+    def setUp(self):
+        self.start_time = "10:13 AM"
+        self.timesheet = Timesheet()
+        self.timesheet.add_time(self.start_time)
+
+    def test_current_task_should_return_something(self):
+        self.assertIsNotNone(self.timesheet.current_task)
+
+
+    def test_current_task_should_have_correct_start_time(self):
+        self.assertEqual(self.start_time,
+                         self.timesheet.current_task.start.strftime('%I:%M %p'))
+
--- timesheet/timesheet.py
+++ timesheet/timesheet.py
@@ -1,9 +1,35 @@
-from datetime import datetime
+import datetime
 
+class Task(object):
+    def __init__(self, start, end, date):
+        self.start = start
+        self.end = end
+        self.date = date
 
 class Timesheet(object):
+    def __init__(self):
+        self.tasks = []
+
     def time_spent(self):
         return 0
 
-    def add_time(self, start):
-        start = datetime.strptime(start, "%I:%M %p")
+    def add_time(self, start, end=None, task=None, proj=None, date=None):
+        start = datetime.datetime.strptime(start, "%I:%M %p")
+        if end is not None:
+            end = datetime.datetime.strptime(end, "%I:%M %p")
+        if date is None:
+            date = datetime.date.today()
+        else:
+            date = datetime.datetime.strptime(date, "%Y-%m-%d").date()
+
+        task = Task(start, end, date)
+        self.tasks.append(task)
+
+
+    @property
+    def current_task(self):
+        for task in self.tasks:
+            if task.end is None:
+                return task
+        
+        return None
